962df89f0ee3554b709eeb3078e8341f178e67ad
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-04 20:59:54 +1000

    Рекурсия

--- Example0408ver1/src/Main.java
+++ Example0408ver1/src/Main.java
@@ -1,38 +1,15 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[10];
-        int a = -100, b = 200;
-        for (int i = 0; i < array.length; i++) {
-            // Случайное целое число в диапазоне от а до b
-            array[i] = (int)((b - a) * Math.random() + a);
-        }
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input item");
-        int item = scanner.nextInt();
-        System.out.println(binarySearch(array, item));
+        System.out.println(10);
+        System.out.println(fact(3));
     }
 
-    private static int binarySearch(int[] array, int item) {
-        int low = 0;                    // нижняя граница поиска
-        int high = array.length - 1;    // верхняя граница поиска
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int guess = array[mid];
-            if (guess == item) {        // элемент найден
-                return mid;
-            }
-            if (guess > item) {         // "Много"
-                high = mid - 1;
-            } else {
-                low = mid + 1;          // "Мало"
-            }
+    private static int fact(int n) {
+        if (n == 1) {
+            return 1;
         }
-        return -1;
+        return fact(n - 1) * n;
     }
+
 }
