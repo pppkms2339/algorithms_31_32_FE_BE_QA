78582f4353affafd2071cb030f8997d96b9cbf47
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-18 20:18:57 +1000

    Реализация очереди

--- Example0408ver1/src/Main.java
+++ Example0408ver1/src/Main.java
@@ -1,28 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        Stack st1 = new Stack();
-        String[] str = {"1","C"};
-        for (int i = 0; i < str.length; i++) {
-            if (str[i] == "C") {
-                st1.pop();
-            } else if (str[i] == "D") {
-                st1.push(2 * st1.top());
-            } else if (str[i] == "+") {
-                int a = st1.pop();
-                int b = st1.top();
-                st1.push(a);
-                st1.push(a + b);
-            } else {
-                st1.push(Integer.parseInt(str[i]));
-            }
-
+        Queue queue = new Queue();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.delete());
         }
-        int sum = 0;
-        while (!st1.isEmpty()) {
-            sum += st1.pop();
-        }
-        System.out.println(sum);
+        queue.add(40);
+        System.out.println(queue.delete());
     }
 
 }
