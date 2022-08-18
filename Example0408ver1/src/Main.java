9f25681f8f7194e4a26ac2657829c8eb280f5691
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-18 18:40:51 +1000

    Решение домашнего задания Level 1

--- Example0408ver1/src/Main.java
+++ Example0408ver1/src/Main.java
@@ -1,15 +1,51 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(10);
-        System.out.println(fact(3));
+        Stack st1 = new Stack();
+        char[] brackets = {'(', ')', '[', ']', '{', '}'};
+        String str = "(([{]}))";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isOpenBracket(c, brackets)) {
+                st1.push(c);
+            } else {
+                if (st1.isEmpty()) {
+                    System.out.println("Wrong bracket string");
+                    return;
+                }
+                char b = st1.pop();
+                // Совпадение типов скобок b и c
+                if (!isBracketsMatch(b, c, brackets)) {
+                    System.out.println("Wrong bracket string");
+                    return;
+                }
+            }
+        }
+        if (st1.isEmpty()) {
+            System.out.println("Correct bracket string");
+        } else {
+            System.out.println("Wrong bracket string");
+        }
     }
 
-    private static int fact(int n) {
-        if (n == 1) {
-            return 1;
+    private static boolean isOpenBracket(char c, char[] brackets) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (brackets[i] == c) {
+                return true;
+            }
         }
-        return fact(n - 1) * n;
+        return false;
+    }
+
+    // b - открывающаяся скобка
+    // c - закрывающаяся скобка
+    private static boolean isBracketsMatch(char b, char c, char[] brackets) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (brackets[i] == b && brackets[i + 1] == c) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
---
+++ Example0408ver1/src/Stack.java
@@ -0,0 +1,27 @@
+public class Stack {
+
+    private StackItem top;
+
+    public void push(char item) {
+        StackItem temp = new StackItem();
+        temp.value = item;
+        temp.next = top;
+        top = temp;
+    }
+
+    public char pop() {
+        char a = top.value;
+        top = top.next;
+        return a;
+    }
+
+    // Проверка пуст ли стек
+    public boolean isEmpty() {
+        return (top == null);
+    }
+
+    private class StackItem {
+        char value;
+        StackItem next;
+    }
+}
