3317b8a11be4cfadcc71573959fbc9d5d404ff92
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-25 23:03:36 +1000

    Тестирование дека

--- Example2508/src/Main.java
+++ Example2508/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         Deque deque = new Deque();
+        deque.addFirst(10);
+        deque.addLast(20);
+        deque.addLast(30);
+        deque.addFirst(0);
+        System.out.println(deque.deleteLast());     // 30
+        System.out.println(deque.deleteFirst());    // 0
+        while (!deque.isEmpty()) {
+            System.out.println(deque.deleteFirst());    // 10 20
+        }
+        System.out.println(deque.isEmpty());    // true
     }
 }
